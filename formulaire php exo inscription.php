8b75a86752874c3ea2bcf04dfbd1b4fe628d2d59
Ivann MIAVOUDILA <MIAVOUDILA_Iv@lprs.local>
2024-11-29 11:04:51 +0100

    feat(): creation du CRUD
    -formulaire php exo inscription.html
    -formulaire php exo inscription.php
    -formulaire php exo connexion.html
    -formulaire php exo connexion.php

---
+++ formulaire php exo inscription.php
@@ -0,0 +1,37 @@
+<?php
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $email = $_POST["email"];
+    $mdp = $_POST["mdp"];
+    $age = $_POST["age"];
+    $pays = $_POST["pays"];
+    $metier = $_POST["metier"];
+}
+
+$formulaire = array(
+    'Nom' => $nom,
+    'Prenom' => $prenom,
+    'Email' => $email,
+    'Password' => $mdp,
+    'Age' => $age,
+    'Pays' => $pays,
+    'Metier' => $metier
+);
+
+foreach ($formulaire as $key => $element) {
+    echo $key . " : " . $element . '<br>';
+}
+
+$bdd = new PDO('mysql:host=localhost;dbname=miavoudila', 'root', '');
+
+$req = $bdd->prepare('INSERT INTO personne(nom,prenom,email,Passord,Age,Pays,Metier) VALUES(:nom,:prenom)');
+$req->execute(array(
+    'Nom' => $nom,'Prenom' => $prenom,'Email' => $email, 'Password' => $mdp, 'Age' => $age,'Pays' => $pays, 'Metier' => $metier
+));
+
+echo 'Bonjour '. $nom . ', vous venez de vous inscrire. Bienvenue dans notre site';
+
+echo '<a href="http://localhost/MIAVOUDILA%20Ivann/formulaire%20php%20exo/formulaire%20php%20exo%20connexion.html">Allez vous connecter ;)</a>';
