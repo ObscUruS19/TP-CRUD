ce9690cbff37fc3798262cd4bb947a7bab42ec1a
Soufiane OUHAIMMED <OUHAIMMED_So@lprs.local>
2024-12-04 09:28:16 +0100

    feat(connexion):réalisation du formulaire de connexion        -formulaire php exo connexion.php

--- formulaire php exo connexion.php
+++ formulaire php exo connexion.php
@@ -1,1 +1,22 @@
 <?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $mdp = $_POST["mdp"];
+}
+
+
+$formulaire = array(
+    'Nom' => $nom,
+    'Password' => $mdp,
+);
+
+foreach ($formulaire as $key => $element) {
+    echo $key . " : " . $element . '<br>';
+}
+
+$bdd = new PDO('mysql:host=localhost;dbname=miavoudila', 'root', '');
+
+echo 'Bonjour '. $nom . ', vous venez de vous connecter. Bienvenue dans notre site';
+
+echo '<a href="http://localhost/MIAVOUDILA%20Ivann/formulaire%20php%20exo/formulaire%20php%20exo%20inscription.html">Allez vous inscrire ;)</a>';
